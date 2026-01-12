92072268ef43788e08f846cea71d786d458faa82
PranavDivvela <divvelapranav@gmail.com>
2026-01-12 18:07:19 -0500

    Debugging
    Few edits: since the code was giving errors and wasn't producing any plot outputs. Edited few lines to debug the code and get it running.

--- code/ALDEx2_3-code.R
+++ code/ALDEx2_3-code.R
@@ -1,25 +1,31 @@
 # ALDEx2
 
-load('data/kin.Rda')
-load("data/pup.Rda")
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/kin.Rda')
+load("~/Desktop/3383/0_git/tiyani-aldex3/data/pup.Rda")
 
 library(ALDEx2)
 conds <- c(rep("k",103), rep("P",161))
-KP.0 <- aldex(cbind(kin,pup), conditions=conds, gamma=0.2)
-aldex.plot(KP.0) # effect plot
-aldex.plot(KP.0, type='volcano')
+#KP.0 <- aldex(cbind(kin,pup), conditions=conds, gamma=0.2)
+#aldex.plot(KP.0) # effect plot
+#aldex.plot(KP.0, type='volcano')
+print("ALDEX2-done")
 
 # ALDEx3
 # loading a non-compiled version from local github repository
-devtools::load_all('~/Documents/0_git/projects/ALDEx3')
+#devtools::load_all('~/Documents/0_git/projects/ALDEx3')
+
 
 library(ALDEx3)
-#      Y <- matrix(1:110, 10, 11)
-#      condition <- c(rep(0, 5), rep(1, 6))
-#      data <- data.frame(condition=condition)
+      Y <- matrix(1:110, 10, 11)
+      condition <- c(rep(0, 5), rep(1, 6))
+      data <- data.frame(condition=condition)
 #      ## demonstrate formula interface and passing optional argument (gamma) to
 #      ## the scale model (clr)
-#      res <- aldex(Y, ~condition, data, nsample=2000, scale=clr.sm, gamma=0.5)
+      res <- ALDEx3::aldex(Y, ~condition, data, nsample=2000, scale=clr.sm, gamma=0.5)
+
+
+print("Test_Block_1")
+
 
 # make the data matrix via cbind
 Y <- cbind(kin,pup)
@@ -28,12 +34,16 @@ Y <- cbind(kin,pup)
 conds <- c(rep("K", 103), rep('P',161))
 data <- data.frame(condition=conds)
 
+print("Test_block_2")
 
 # does the calculation
-res <- aldex(Y, ~condition, data, nsample=128, scale=clr.sm, gamma=1e-3)
+res <- ALDEx3::aldex(Y, ~condition, data, nsample=128, scale=clr.sm, gamma=1e-3)
 
 # summarize
-sum.0 <- summary.aldex(res)
+# The command was giving a error "could not find the function" so, It has been updated to newer command 
+# summary.aldex() --> summary()
+sum.0 <- summary(res)
+print("Test_Block_3")
 
 # there is not a native plotting function yet!!
 plot(sum.0$std.error, sum.0$estimate)
@@ -41,17 +51,23 @@ sig <- which(sum.0$p.val.adj < 0.05)
 points(sum.0$std.error[sig], sum.0$estimate[sig], col='red', pch=19, cex=0.5)
 abline(h=0)
 
-# now do for gamma of 0.3
-res <- aldex(Y, ~condition, data, nsample=128, scale=clr.sm, gamma=0.3)
+print("Test_block_4")
 
-sum.3 <- summary.aldex(res)
-plot(sum.3$std.error, sum.3$estimate, col="grey", pch=19, cex=0.5)
-sig3 <- which(sum.3$p.val.adj < 0.05)
-# these are signficant without gamma
-points(sum.3$std.error[sig], sum.3$estimate[sig], col='orange', pch=19, cex=0.5)
-# these are signficant with gamma
-points(sum.3$std.error[sig3], sum.3$estimate[sig3], col='red', pch=19, cex=0.5)
-abline(h=0)
+## now do for gamma of 0.3
+#res <- ALDEx3::aldex(Y, ~condition, data, nsample=128, scale=clr.sm, gamma=0.3)
+
+## The command was giving a error "could not find the function" so, It has been updated to newer command 
+## summary.aldex() --> summary()
+#sum.3 <- summary(res)
+#plot(sum.3$std.error, sum.3$estimate, col="grey", pch=19, cex=0.5)
+#sig3 <- which(sum.3$p.val.adj < 0.05)
+
+## these are signficant without gamma
+#points(sum.3$std.error[sig], sum.3$estimate[sig], col='orange', pch=19, cex=0.5)
+
+## these are signficant with gamma
+#points(sum.3$std.error[sig3], sum.3$estimate[sig3], col='red', pch=19, cex=0.5)
+#abline(h=0)
 
 
 
