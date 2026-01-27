9c9ee52c90e59b613c8bb11f268e841a3ccec24c
PranavDivvela <divvelapranav@gmail.com>
2026-01-27 00:39:26 -0500

    TEST Code_ with age only datasets and as a discrete variable.

---
+++ code/Test_Pairwise_with_Age.R
@@ -0,0 +1,62 @@
+# Single Pairwise ALDEx3 Analysis - Test Run
+
+library(ALDEx2)
+library(ALDEx3)
+
+# Load datasets (count data)
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/kin.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/pup.Rda')
+
+# Load age metadata
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/kin.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/pup.age.Rda')
+
+
+Y <- cbind(kin, pup)
+
+# make a conditions vector
+# for a continuous variable (age) we replace this with the ages of the samples
+age <- c(as.numeric(kin.age[1,]), as.numeric(pup.age[1,]))
+conds <- c(rep("K", length(kin.age)), rep('P', length(pup.age)))
+data <- data.frame(age = age, condition = conds)
+
+# Check counts
+print("Condition counts:")
+print(table(conds))
+
+
+# Set seed for reproducibility
+set.seed(42)
+
+# Run ALDEx3 with gamma = 0.3
+print("Running ALDEx3 analysis...")
+
+res_gamma <- ALDEx3::aldex(Y, ~condition, data, nsample = 128, scale = clr.sm, gamma = 0.3)
+sum_gamma <- summary(res_gamma)
+
+# Get significant taxa
+sig_gamma <- which(sum_gamma$p.val.adj < 0.05)
+
+print(paste("Significant taxa:", length(sig_gamma)))
+
+
+# Plot
+plot(sum_gamma$std.error, sum_gamma$estimate,
+     main = paste(name1, "vs", name2),
+     sub = paste0("Gamma = 0.3 | Ages: ", min(age1), "-", max(age1), 
+                  " vs ", min(age2), "-", max(age2),
+                  " | n_sig = ", length(sig_gamma)),
+     xlab = "Std Error", ylab = "Estimate", 
+     col = "black", pch = 1)
+
+# Highlight significant points
+points(sum_gamma$std.error[sig_gamma], sum_gamma$estimate[sig_gamma], 
+       pch = 19, col = 'red', cex = 0.5)
+abline(h = 0, lty = 2)
+
+# Add legend
+legend("topright", 
+       legend = c("Not significant", "Significant (p.adj < 0.05)"),
+       col = c("black", "red"), 
+       pch = c(1, 19), 
+       cex = 0.8)
--- code/gg-aldex-age-cond.R
+++ code/gg-aldex-age-cond.R
@@ -1,3 +1,18 @@
+library(ALDEx2)
+library(ALDEx3)
+
+# Load all datasets
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/cent.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/eld.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/kin.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/mage.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/mid.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/pup.age.Rda')
+load('~/Desktop/3383/0_git/tiyani-aldex3/data/you.age.Rda')
+
+
+
+
 Y <- cbind(kin,pup)
 # make a conditions vector
 # for a continuous variable (age) we replace this with the ages of the samples
@@ -11,7 +26,7 @@ print("Test_block_2")
 #  ## Had to add "ALDEx3::aldex()" to make the code work: for some reason it was not able to work 
 #  ## and started giving me the "Error in round(conds) : non-numeric argument to mathematical function"
 #  ## Till I made that change
-res.age <- ALDEx3::aldex(Y, ~age, data, nsample=128, scale=clr.sm, gamma=1e-3)
+res.age <- ALDEx3::aldex(Y, ~age, data, nsample=128, scale=clr.sm, gamma=0.3)
 res.conds <- ALDEx3::aldex(Y, ~condition, data, nsample=128, scale=clr.sm, gamma=1e-3)
 
 # summarize
@@ -25,9 +40,10 @@ sum.0a <- summary(res.age)
 print("Test_Block_3")
 
 sig <- which(sum.0$p.val.adj < 0.05)
+siga <- which(sum.0a$p.val.adj < 0.05)
 
 plot(sum.0a$std.error, sum.0a$estimate)
-siga <- which(sum.0a$p.val.adj < 0.05)
+
 points(sum.0a$std.error[sig], sum.0a$estimate[sig], col='red', pch=19, cex=0.5)
 points(sum.0a$std.error[siga], sum.0a$estimate[siga], col='orange', pch=19, cex=0.5)
 abline(h=0)
